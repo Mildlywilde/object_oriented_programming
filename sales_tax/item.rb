91b2d071712b2ddd4adc1b64965d9fc07b4a8384
Jack WIlde <jackwilde630@gmail.com>
2016-08-12 15:05:20 -0400

    calculate sales tax

--- sales_tax/item.rb
+++ sales_tax/item.rb
@@ -1,10 +1,11 @@
 class Item
 
-  attr_reader :tax_exempt, :imported
-  
-  def initialize(tax_exempt, imported)
+  attr_reader :tax_exempt, :imported, :value
+
+  def initialize(tax_exempt, imported, value)
     @tax_exempt = tax_exempt
     @imported = imported
+    @value = value
   end
 
 end
--- sales_tax/receipt.rb
+++ sales_tax/receipt.rb
@@ -1,11 +1,20 @@
 require_relative 'item.rb'
 
-class Receiept
+class Receipt
 
   def initialize(item)
     @item = item
   end
 
-  
+  def calculate_tax
+    if @item.tax_exempt == true
+      price = @item.value
+    else
+      price = @item.value + (@item.value / 10.0)
+    end
+    puts price
+  end
+
+
 
 end
